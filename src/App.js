cd6e2b6620f67ceb27be680093488e6579494736
SohailAsghar <Sohailkundi6651@gmail.com>
2024-08-30 17:59:56 -0700

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    
+  <>
+  
+  <Navbar AboutText="About US" Home="sohail"/>
+  </>
+  );
+}
+
+export default App;
+
+
+// style={{ backgroundImage: 'url(background.png)', backgroundSize: 'cover', backgroundPosition: 'center',ackgroundAttachment: 'fixed'  }}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+      {/* <img src="background.png" alt="" /> */}
+    </div>
+    <div className="header" >
+      <div className='nk-contacts-hrefp' >
+        <div className='container'>
+          <div className='row '>
+
+            <div className='nk-contacts-left col-md-8'>
+              <div className='nk-social-links'>
+                <ul>
+
+                  <li>
+                    <a className="fa fa-rss" href="#" style={{ size: "30px" }}></a>
+                  </li>
+                  <li>
+                    <a className="fa fa-twitch" href="#" style={{ size: "30px" }}></a>
+                  </li>
+                  <li>
+                    <a className="fa fa-twitter" href="#" style={{ size: "30px" }}></a>
+                  </li>
+                  <li>
+                    <a className="fa fa-steam" href="#" style={{ size: "30px" }}></a>
+                  </li>
+                  <li>
+                    <a className="fa fa-facebook" href="#" style={{ size: "30px" }}></a>
+                  </li>
+                  <li>
+                    <a className="fa fa-pinterest" href="#" style={{ size: "30px" }}></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='nk-contacts-Right '>
+                <div className='nk-contacts-Icons d-flex'>
+                  <a href="#">
+                    <i className="fa fa-user" style={{ fontSize: "15px" }}></i>
+                  </a>
+                  <a href="#">
+                    <i className="fa fa-search" style={{ fontSize: "15px" }}></i>
+                  </a>
+                  <a href="#">
+                    <i className="fa fa-shopping-cart" style={{ fontSize: "15px" }}>
+                      <span className="nk-badge">27</span>
+                    </i>
+                  </a>
+                </div>
+              </div>
+          </div>
+
+       </div>
+    </div>
+     </div>
+    </>
+  )
+}
+
+export default Navbar
